3abfa84dabec1c1905e573b4a526fef20e296e10
David <dovelx@163.com>
2020-05-28 14:31:29 +0800

    hourly update

--- untitled3/selenium_tets/main.py
+++ untitled3/selenium_tets/main.py
@@ -199,16 +199,16 @@ formdatafenxi ={
 	"wf_create_user": 1000,
 	"iscontractor": "0",
 	"analyze_type": "jsa,aqjd",
-	"work_appoint_name": "create_by_python_JOUsxk",
+	"work_appoint_name": name,
 	"territorialunitid": 2000000003339,
 	"territorialunitname": "运行一部",
 	"workstatus": "draft",
 	"dataStatus": 0,
 	"ver": 1,
 	"created_by": 1000,
-	"created_dt": "2020-05-27 17:32:05",
+	"created_dt": now,
 	"updated_by": 1000,
-	"updated_dt": "2020-05-27 17:32:05",
+	"updated_dt": now,
 	"df": 0,
 	"tenantid": 1,
 	"ts": "",
@@ -223,8 +223,8 @@ formdatafenxi ={
 	"worktickettype_name": "作业许可证,动火作业",
 	"workunitname": "长庆石化分公司",
 	"workunit": 1688712,
-	"planstarttime": "2020-05-26 18:47:50",
-	"planendtime": "2020-05-26 18:52:50",
+	"planstarttime":fnow1,
+	"planendtime": fnow2,
 	"site": "作业地点123",
 	"equipmentname": "",
 	"work_position_name": "制氢北区",
@@ -240,6 +240,233 @@ rs=requests.post(urlfenxi, json = formdatafenxi, headers = headers,cookies=cooki
 rs.encoding='utf-8'
 cc = str(rs.content, 'utf8')
 print(cc)
+#预约安全分析接口地址
+url11 = 'http://192.168.6.27:6030/hse/HSE_SAFETY_TASK_RISK/getMetaData?0.26386458099914045&contentType=json&ajax=true&tid=1'
+
+#请求接口
+rs=requests.get(url11, headers = headers,cookies=cookies)
+#返回值转码
+data = rs.content.decode('utf-8')
+#json化
+data = json.loads(data)
+#获取接口返回状态
+sta= data['status']
+
+print (sta)
+a = data['data']["voset"]["voList"]
+b =[]
+for i in range(len(a)):
+
+    if a[i]['worktaskid'] !="" and a[i]['worktaskid'] !="None":
+        b.append(a[i]['worktaskid'])
+#print (b)
+num1 = num
+print (num1)
+#print (max(b))
+#安全分析步骤添加
+url ='http://192.168.6.27:6030/hse/HSE_SAFETY_ANALYSIS_STEP_RISK/cardSave?parentEntityId=%d&parentFuncCode=HSE_SAFETY_ANALYSIS_RISK&topEntityId=%d&topFuncCode=HSE_SAFETY_TASK_RISK&0.5426692795870303&contentType=json&ajax=true&tid=1'%(num1,num1)
+data = {
+	"tableName": "hse_safety_analysis_step",
+	"qualify_level": "no_qualify",
+	"duty_name": "",
+	"jsaid": num1,
+	"dataStatus": 0,
+	"ver": 1,
+	"created_by": "",
+	"created_dt": now,
+	"updated_by": "",
+	"updated_dt": now,
+	"df": 0,
+	"tenantid": 1,
+	"ts": "",
+	"step_type": "01",
+	"evaluate_type": "",
+	"risk_level": "02",
+	"remain_risk_accept": "",
+	"risk_value": 0,
+	"risk_harm": "风险及危害123",
+	"consequence": "后果123",
+	"gravity": "1",
+	"accident_possibility": "1",
+	"step_name": "步骤活动123"
+}
+#请求接口
+rs=requests.post(url, json = data, headers = headers,cookies=cookies)
+rs.encoding='utf-8'
+cc = str(rs.content, 'utf8')
+#print(cc)
+#time.sleep(5)
+#安全分析保存
+#num1 = num1
+print(num1)
+url='http://192.168.6.27:6030/hse/HSE_SAFETY_ANALYSIS_RISK/cardSave?parentEntityId=%d&parentFuncCode=HSE_SAFETY_TASK_RISK&topEntityId=%d&topFuncCode=HSE_SAFETY_TASK_RISK&dataId=%d&0.2955948527813328&contentType=json&ajax=true&tid=1'%(num1,num1,num1)
+data ={
+	"tableName": "hse_safety_analysis",
+	"updated_by_name": "测试用户",
+	"create": "",
+	"dataStatus": 0,
+	"ver": 1,
+	"created_by": 1000,
+	"created_dt": now,
+	"updated_by": 1000,
+	"updated_dt": now,
+	"df": 0,
+	"tenantid": 1,
+	"ts": "",
+	"jsaid": num1,
+	"jsa_templete_name": "安全分析模板",
+	"jsa_templete_id": num1,
+	"temp_type": "newWorkTask",
+	"jsa_monitor_userid": 1000,
+	"jsa_monitor_name": "测试用户",
+	"jsa_menber_userids": "1000",
+	"jsa_menber_username": "测试用户",
+	"analyze_time": "2020-05-29 13:04:16",
+	"worktickettype": "",
+	"equip_stuff": "",
+	"worktaskid": num1,
+	"workstatus": "draft",
+	"worktype": "jsa",
+	"revampandadvide": "",
+	"inspection_name": "",
+	"work_position_id": 2000000002019,
+	"projecttype": "",
+	"workname": "",
+	"workunitname": "",
+	"reference": "",
+	"iscontractor": "",
+	"territorialunitid": "",
+	"territorialunitname": "",
+	"planendtime": "",
+	"reviewer": "",
+	"site": "",
+	"worknumber": "",
+	"workunit": "",
+	"craftprocess": "",
+	"planstarttime": "",
+	"workcontent": "",
+	"isnew": "",
+	"wf_instance": "",
+	"wf_current_user": "",
+	"wf_audit_time": "",
+	"wf_current_nodeid": "",
+	"wf_type": "",
+	"wf_create_user": "",
+	"wf_audit_state": "",
+	"sourcejsaid": "",
+	"remainsrisk_level": "",
+	"risk_level": "04"
+}
+
+#请求接口
+rs=requests.post(url, json = data, headers = headers,cookies=cookies)
+rs.encoding='utf-8'
+cc = str(rs.content, 'utf8')
+print(cc)
+#安全交底
+num1 = num+20
+url='http://192.168.6.27:6030/hse/HSE_SAFETY_DISCLOSURE/cardSave?parentEntityId=%d&parentFuncCode=HSE_SAFETY_TASK_RISK&topEntityId=%d&topFuncCode=HSE_SAFETY_TASK_RISK&dataId=%d&0.7447101068947941&contentType=json&ajax=true&tid=1'%(num,num,num1)
+data = {
+	"tableName": "hse_safety_disclosure",
+	"additional_content": "",
+	"confirm_content": "1、已清楚作业区域及周边生产作业情况\r\n2、已清楚本次作业的安全风险（JSA）\r\n3、已清楚本次作业的具体安全要求（作业许可证中的控制措施）\r\n4、已对本次作业现场安全措施进行了检查确认\r\n5、已清楚本次作业涉及的作业许可证的有限期限 \r\n6、已掌握个人防护用具正确佩戴使用方法\r\n7、已清楚突发情况下的应急避险方法",
+	"dataStatus": 0,
+	"ver": 1,
+	"created_by": 1000,
+	"created_dt": now,
+	"updated_by": 1000,
+	"updated_dt": now,
+	"df": 0,
+	"tenantid": 1,
+	"ts": "",
+	"safeclarid": num1,
+	"projecttype": "",
+	"safe_name": "长庆石化安全交底",
+	"worktype": "aqjd",
+	"workstatus": "draft",
+	"scopeandenv": "",
+	"workrisk": "",
+	"preventmeasure": "",
+	"emermeasure": "",
+	"othermatter": "",
+	"safe_content": "长庆石化安全交底",
+	"safe_clar_temp_id": 2000000001040,
+	"safe_clar_temp_name": "",
+	"worktaskid": num,
+	"work_position_id": 2000000002019
+}
+#请求接口
+rs=requests.post(url, json = data, headers = headers,cookies=cookies)
+rs.encoding='utf-8'
+cc = str(rs.content, 'utf8')
+print(cc)
+
+#安全送交
+url = 'http://192.168.6.27:6030/hse/HSE_SAFETY_TASK_RISK/wfSend?parentEntityId=&parentFuncCode=&topEntityId=%d&topFuncCode=HSE_SAFETY_TASK_RISK&dataId=%d&0.9498759321537273&contentType=json&ajax=true&tid=1'%(num,num)
+data = {}
+#请求接口
+rs=requests.post(url, json = data, headers = headers,cookies=cookies)
+rs.encoding='utf-8'
+cc = str(rs.content, 'utf8')
+print(cc)
+
+#作业任务添加
+url = 'http://192.168.6.27:6030/hse/HSE_WORK_TASK_MCQ/cardSave?parentEntityId=&parentFuncCode=&topFuncCode=HSE_WORK_TASK_MCQ&0.9079012038155838&contentType=json&ajax=true&tid=1'
+data  = {
+	"tableName": "hse_work_task",
+	"iscontractor": "0",
+	"isupgrade": "0",
+	"work_appoint_name": name,
+	"territorialunitid": 2000000003339,
+	"applyunitname": "长庆石化分公司",
+	"task_pause": "0",
+	"territorialunitname": "运行一部",
+	"territorialunitcode": "CS8082020",
+	"applyunitid": "1688712",
+	"workstatus": "draft",
+	"autorisklevel": 1,
+	"dataStatus": 0,
+	"ver": 1,
+	"created_by": "",
+	"created_dt": now,
+	"updated_by": "",
+	"updated_dt": now,
+	"df": 0,
+	"tenantid": 1,
+	"ts": "",
+	"projecttype": "rcjx",
+	"isrecord": "",
+	"eq_position": "",
+	"territorialdeviceid": 2000000003454,
+	"territorialdevicename": "制氢装置",
+	"jsaid": 2000000001860,
+	"work_appoint_id": 2000000001860,
+	"jsa_code": "Created_by_Python_HmNEGR",
+	"site": "作业地点123",
+	"workunit": 1688712,
+	"workunitname": "长庆石化分公司",
+	"work_position_id": 2000000002019,
+	"work_position_name": "制氢北区",
+	"workcontent": "作业内容123",
+	"planstarttime": fnow1,
+	"planendtime": fnow2,
+	"standardmaintenance_name": "",
+	"constructionscheme": 0,
+	"worktickettype": "xkz,dh",
+	"worktickettype_name": "作业许可证,动火作业",
+	"standardmaintenance": "",
+	"equipmentname": "",
+	"equipmentnumber": "",
+	"equipmentcode": "",
+	"workname": name
+}
+#请求接口
+rs=requests.post(url, json = data, headers = headers,cookies=cookies)
+rs.encoding='utf-8'
+cc = str(rs.content, 'utf8')
+print(cc)
+#作业任务提交
+url =  'http://192.168.6.27:6030/hse/HSE_WORK_TASK_MCQ/hse_work_task_submit?parentEntityId=&parentFuncCode=&topEntityId=2000000004152&topFuncCode=HSE_WORK_TASK_MCQ&dataId=2000000004152&0.7819922897402813&contentType=json&ajax=true&tid=1'
 time.sleep(2)
 driver.close()
 driver.quit()
