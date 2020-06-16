43897b642335f52924dd11950a2ca6a660462114
David <dovelx@163.com>
2020-06-16 14:39:14 +0800

    长庆项目作业任务添加功能，添加新字段，安全分析图片，暂停编写

--- interface_project_for_dev/debug101.py
+++ interface_project_for_dev/debug101.py
@@ -1,5 +1,43 @@
-from tools import msql
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import requests
+import random
+import string
+import datetime
+from selenium.webdriver import Chrome, ChromeOptions
 
-a = msql.sql_query_worktaskid("Created_by_Python_M85JV0")
 
-print(a)
+#opt = ChromeOptions()            # 创建Chrome参数对象
+#opt.headless = True              # 把Chrome设置成可视化无界面模式，windows/Linux 皆可
+#driver = Chrome(options=opt)     # 创建Chrome无界面对象
+#selenium登录测试长庆
+#driver = webdriver.Firefox()
+driver = webdriver.Chrome()
+driver.get("http://192.168.6.27:6030/passports/login?service=http%3A%2F%2F192.168.6.27%3A6030%2Fportals%2Fcas&tenantCode=cqsh&trial=false")
+
+driver.find_element(By.ID, "username").send_keys("test")
+driver.find_element(By.ID, "pwd1").send_keys("1")
+driver.find_element(By.CSS_SELECTOR, ".justUse").click()
+
+
+#获取JSESSIONID
+c= driver.get_cookies()
+#print (c)
+print (c[0])
+for a in c:
+    #print (a)
+    if a['name'] == 'JSESSIONID':
+        b=a
+        #print (b)
+cookies={'JSESSIONID': b['value']}
+
+#cookies={'JSESSIONID': '3BAB7DF0381948EA376F907859D5321C'}
+print(cookies)
--- interface_project_for_dev/runners/pc_login.py
+++ interface_project_for_dev/runners/pc_login.py
@@ -1,3 +1,36 @@
-
 # 临时cookies
-cookies = {'JSESSIONID': 'DC296D825A86596378F0F0CE1B043621yGe1TA'}
+cookies = {'JSESSIONID': '1DB98F9F643BCB62D8DF361111E774B8HitJ9f'}
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Firefox,FirefoxOptions
+def login():
+    opt = FirefoxOptions()            # 创建Chrome参数对象
+    opt.headless = True              # 把Chrome设置成可视化无界面模式，windows/Linux 皆可
+    driver = Firefox(options=opt)     # 创建Chrome无界面对象
+    #selenium登录测试长庆
+    #driver = webdriver.Firefox()
+
+    driver.get("http://192.168.6.27:6030/passports/login?service=http%3A%2F%2F192.168.6.27%3A6030%2Fportals%2Fcas&tenantCode=cqsh&trial=false")
+
+    driver.find_element(By.ID, "username").send_keys("test")
+    driver.find_element(By.ID, "pwd1").send_keys("1")
+    driver.find_element(By.CSS_SELECTOR, ".justUse").click()
+
+
+    #获取JSESSIONID
+    c= driver.get_cookies()
+    #print (c)
+    #print (c[0])
+    for a in c:
+        #print (a)
+        if a['name'] == 'JSESSIONID':
+            b=a
+            #print (b)
+    cookies={'JSESSIONID': b['value']}
+
+    #cookies={'JSESSIONID': '3BAB7DF0381948EA376F907859D5321C'}
+    return cookies
+
+#cookies= login()
+
+print(cookies)
